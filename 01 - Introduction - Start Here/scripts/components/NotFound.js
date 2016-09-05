b6672ce6f7b4766a057295e378397e116e983891
Christine Movius <christine.movius@gmail.com>
2016-09-04 22:52:17 -0400

    Complete migratining components to individual files.

--- 01 - Introduction - Start Here/scripts/components/NotFound.js
+++ 01 - Introduction - Start Here/scripts/components/NotFound.js
@@ -1,9 +1,14 @@
+/*
+  Not Found
+*/
+
 import React from 'react';
 
 var NotFound = React.createClass({
-  render: function() {
-    return (<h2>Not Found!</h2>)
+  render : function() {
+    return <h1>Not Found!</h1>
   }
-})
+});
+
 
 export default NotFound;
--- 01 - Introduction - Start Here/scripts/components/StorePicker.js
+++ 01 - Introduction - Start Here/scripts/components/StorePicker.js
@@ -1,26 +1,30 @@
+/* 
+  StorePicker
+  This will let us make <StorePicker/>
+*/
+
 import React from 'react';
 import { History } from 'react-router';
-import helpers from '../helpers.js';
+import h from '../helpers';
 
 var StorePicker = React.createClass({
-  mixins: [History],
-  goToStore: function(event) {
+  mixins : [History],
+  goToStore : function(event) {
     event.preventDefault();
+    // get the data from the input
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
   },
-  render: function() {
+  render : function() {
     return (
-      <div>
-        <form className='store-selector' onSubmit={this.goToStore}>
-          {/*Comments*/}
-          <h2>Please Enter a Store</h2>
-          <input type='text' ref='storeId' required defaultValue={helpers.getFunName()}/>
-          <input type='submit' />
-        </form>
-      </div>
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+        <input type="Submit" />
+      </form>
     )
   }
+
 });
 
 export default StorePicker;
