9aa91d5624a9d46cc8e955d509366b3e2f58615b
Christine Movius <christine.movius@gmail.com>
2016-09-04 23:15:33 -0400

    Convert NotFound and StorePicker into Classes. Use ES7 autobind to reference this.

--- 01 - Introduction - Start Here/scripts/components/StorePicker.js
+++ 01 - Introduction - Start Here/scripts/components/StorePicker.js
@@ -1,4 +1,4 @@
-/* 
+/*
   StorePicker
   This will let us make <StorePicker/>
 */
@@ -6,25 +6,31 @@
 import React from 'react';
 import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins : [History],
-  goToStore : function(event) {
+@autobind
+class StorePicker extends React.Component {
+
+  goToStore(event) {
     event.preventDefault();
     // get the data from the input
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render : function() {
+  }
+
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
-        <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
-        <input type="Submit" />
+      <h2>Please Enter A Store</h2>
+      <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+      <input type="Submit" />
       </form>
     )
   }
 
-});
+}
+
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;
